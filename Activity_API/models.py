622eb3f331c40b1da88b0b3457cd38b77d80e7b4
Pravin Rathod <pravinprathod@outlook.com>
2021-05-06 15:10:18 +0530

    update models.py from activity_api to add new fields

--- Activity_API/models.py
+++ Activity_API/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import datetime
 
 class MyTask(models.Model):
     name = models.CharField(max_length=100)
-    is_done = models.BooleanField(default=False, blank=True,       null=True)
+    created_by = models.CharField(default='Pravin', max_length=100)
+    create_date = models.DateField(default=datetime.date.today(), editable=False)
+    is_done = models.BooleanField(default=False, blank=True, null=True)
     objects = models.Manager()
     def __str__(self):
         return self.name
